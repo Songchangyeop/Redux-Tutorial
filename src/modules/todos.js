0cbbcb172dc6794c529e24f10ba6ecf39cc2355b
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2021-05-18 02:09:12 +0900

    redux tutorial

---
+++ src/modules/todos.js
@@ -0,0 +1,59 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; //인풋 값 변경
+const INSERT = 'todos/INSERT'; // 새로운 todo 등록
+const TOGGLE = 'todos/TOGGLE'; // todo를 체크 / 체크해제함
+const REMOVE = 'todos/REMOVE'; //todo를 제거함
+
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
+
+let id = 3; // insert가 호출될 때마다 1씩 더해집니다.
+export const insert = createAction(INSERT, (text) => ({
+  id: id++,
+  text,
+  done: false,
+  // 위의 return된 값들이 Payload !!
+}));
+
+export const toggle = createAction(TOGGLE, (id) => id);
+
+export const remove = createAction(REMOVE, (id) => id);
+
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '리덕스 기초 배우기',
+      done: true,
+    },
+    {
+      id: 2,
+      text: '리액트와 리덕스 사용하기',
+      done: false,
+    },
+  ],
+};
+
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+  },
+  initialState
+);
+
+export default todos;
